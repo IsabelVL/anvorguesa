0f80d138d12aa913477e11600444418563230088
Alumno <villalobosisabel37@gmail.com>
2019-06-21 11:25:38 -0500

    hola

---
+++ js/app.js
@@ -0,0 +1,10 @@
+ $(document).ready(function(){
+    $('.sidenav').sidenav();
+      $('.tabs').tabs();
+     $('.slider').slider({
+      indicators: false,
+      interval:2000,
+      duration: 2000,
+     });
+  });
+        
